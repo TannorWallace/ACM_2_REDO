3e21cbcf160eea095ac624e055fdc19ecf1ddf9e
Tannor Wallace <tannor.wallace@gmail.com>
2020-04-26 15:41:23 -0600

    Add project files.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustromerId { get; private set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+        public bool MyProperty { get; set; }
+    }
+}
